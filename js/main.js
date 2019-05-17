fe9f9439d75711042ff9c57b9b7d86e1ca68c29d
Diego Gonzales <desarrollador.1@htqasas.com>
2019-05-16 23:39:13 -0500

    Enpezado javascript

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-$("#face").onclik(function(){
+/* variable */
 
 
-})
+let nav = document.getElementById('nav');
+let menu = document.getElementById('enlace');
+
+function menus(){
+    let Desplazamiento_Actual = window.pageXOffset;
+
+    if (Desplazamiento_Actual <= 300){
+        nav.classList.remove('nav2');
+        nav.className=('nav1');
+
+    }else{
+        nav.classList.remove('nav1');
+        nav.className=('nav2');
+    }
+}
+
+window.addEventListener('scroll',function(){
+    console.log(window.pageXOffset);
+    menus();
+});
